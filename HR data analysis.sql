cd3f3ec13a81b235f51aff07c82854d8dd079693
bakrkaff <127227605+bakrkaff@users.noreply.github.com>
2023-05-27 13:02:46 -0400

    Update HR data analysis.sql

--- HR data analysis.sql
+++ HR data analysis.sql
@@ -89,3 +89,53 @@ select year(OrderDate), sum(SubTotal) as Revenue
 from Sales.SalesOrderHeader
 group by year(OrderDate)
 order by year(OrderDate);
+
+-- Retrieve all sales orders and their corresponding order details.
+SELECT h.SalesOrderID, h.OrderDate, d.ProductID, d.OrderQty
+FROM Sales.SalesOrderHeader h
+LEFT JOIN Sales.SalesOrderDetail d ON h.SalesOrderID = d.SalesOrderID;
+
+-- The sales order details and their associated product names for orders placed in 2014.
+SELECT d.SalesOrderID, d.ProductID, p.Name, d.OrderQty, d.UnitPrice
+FROM Sales.SalesOrderDetail d
+RIGHT JOIN Production.Product p ON d.ProductID = p.ProductID
+RIGHT JOIN Sales.SalesOrderHeader h ON d.SalesOrderID = h.SalesOrderID
+WHERE YEAR(h.OrderDate) = 2014;
+
+-- Calculate the total sales amount for each customer.
+SELECT c.CustomerID, c.PersonID, c.AccountNumber, SUM(h.TotalDue) AS TotalSalesAmount
+FROM Sales.Customer c
+INNER JOIN Sales.SalesOrderHeader h ON c.CustomerID = h.CustomerID
+GROUP BY c.CustomerID, c.PersonID, c.AccountNumber;
+
+-- Determine the top-selling products.
+SELECT TOP 10 d.ProductID, p.Name, SUM(d.OrderQty) AS TotalQuantitySold
+FROM Sales.SalesOrderDetail d
+INNER JOIN Production.Product p ON d.ProductID = p.ProductID
+GROUP BY d.ProductID, p.Name
+ORDER BY TotalQuantitySold DESC;
+-- The sales performance by month and year.
+SELECT YEAR(h.OrderDate) AS OrderYear, MONTH(h.OrderDate) AS OrderMonth, SUM(h.TotalDue) AS TotalSalesAmount
+FROM Sales.SalesOrderHeader h
+GROUP BY YEAR(h.OrderDate), MONTH(h.OrderDate)
+ORDER BY OrderYear, OrderMonth;
+-- The customers with the highest number of orders.
+SELECT TOP 10 c.CustomerID, c.PersonID, c.AccountNumber, COUNT(h.SalesOrderID) AS OrderCount
+FROM Sales.Customer c
+INNER JOIN Sales.SalesOrderHeader h ON c.CustomerID = h.CustomerID
+GROUP BY c.CustomerID, c.PersonID, c.AccountNumber
+ORDER BY OrderCount DESC
+
+--The total sales amount for each product category, including products that have no sales.
+SELECT c.Name AS CategoryName, p.Name AS ProductName, SUM(ISNULL(d.LineTotal, 0)) AS TotalSalesAmount
+FROM Production.ProductCategory c
+LEFT JOIN Production.ProductSubcategory sc ON c.ProductCategoryID = sc.ProductCategoryID
+LEFT JOIN Production.Product p ON sc.ProductSubcategoryID = p.ProductSubcategoryID
+LEFT JOIN Sales.SalesOrderDetail d ON p.ProductID = d.ProductID
+GROUP BY c.Name, p.Name;
+
+--The customers who have not placed any orders.
+SELECT c.CustomerID, c.PersonID, c.AccountNumber
+FROM Sales.Customer c
+LEFT JOIN Sales.SalesOrderHeader h ON c.CustomerID = h.CustomerID
+WHERE h.CustomerID IS NULL;
